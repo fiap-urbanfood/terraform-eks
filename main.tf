ac71d028b1cc79a35fd472101e72a0f0cb741238
Giovane Nunes <giovane.nunes@silro.com.br>
2025-03-24 23:26:47 -0300

    remove profile

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
   region = "us-east-1"
-  profile = "terraform-iac"
+  #profile = "terraform-iac"
 }
 
 data "aws_region" "selected" {}
 
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
-  # cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  # token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
@@ -23,7 +21,7 @@ terraform {
     bucket = "iac-urbanfood-tfstates"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    profile = "terraform-iac"
+    #profile = "terraform-iac"
   }
 
   required_providers {
